a198953c16598ebc294bd4b74cbc76015223ebcc
Dimuthu <Sanjuladimuthu2@gmail.com>
2026-01-04 04:44:43 -0800

    Initialize DOM element references in script.js
    Added JavaScript code to select input and result elements by ID for further manipulation. Also removed default text from the result paragraph in index.html to prepare for dynamic updates.

---
+++ script.js
@@ -0,0 +1,4 @@
+const textbox = document.getElementById("textbox");
+const toFahrenheit = document.getElementById("toFahrenheit");
+const toCelsius = document.getElementById("toCelsius");
+const result=document.getElementById("result");
